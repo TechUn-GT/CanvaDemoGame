4af4e510406eef2b6547a4d8fbbf283add7a6311
asantott <asantos@tecnologiatransaccional.com>
2025-10-01 12:30:39 -0600

    Implementacion logica Rewards

--- app/src/main/java/com/techun/canvademogame/GameView.kt
+++ app/src/main/java/com/techun/canvademogame/GameView.kt
@@ -12,18 +12,21 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import androidx.core.graphics.scale
+import kotlin.random.Random
 
 class GameView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : View(context, attrs) {
+    /**
+     * Main Character
+     */
     //Parametros Ajustables
     private val gravity = dp(0.5f)
     private val jumpImpulse = dp(-10f)
     private val hitboxMainCharacter = 0.5f
 
     //Main Character
-    private val mainCharacterBmpSrc =
-        BitmapFactory.decodeResource(resources, R.drawable.main_character)
+    private val mainCharacterBmpSrc = BitmapFactory.decodeResource(resources, R.drawable.main_character)
     private var mainCharacterBmp: Bitmap? = null
 
     //Main Character States
@@ -33,13 +36,44 @@ class GameView @JvmOverloads constructor(
     private var mainCharacterH = 0f
     private var mainCharacterVelY = 0f
 
-    //Debug
+    /**
+     * Rewards
+     */
+
+    //Rewards Parameters
+    private val rewardSpeed = dp(4f)
+    private val rewardSpawnChance = 0.02f
+    private val hitboxRewards = 0.9f
+
+    //Rewards Sprites
+    private val rewardsBmpSrc = BitmapFactory.decodeResource(resources, R.drawable.reward)
+    private var rewardBmp: Bitmap? = null
+
+    //Rewards States
+    private var rewardWidth = 0f
+    private var rewardHeight = 0f
+
+    //Reward Pojo
+    data class Reward(var x: Float, var y: Float, var alive: Boolean = true)
+
+    private val rewards = mutableListOf<Reward>()
+
+    /**
+     * Debug
+     */
     private val showHitbox = true
     private val mainCharacterRect = RectF()
-    private val hitPaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+    private val rewardRect = RectF()
+    private val hitMainCharacter = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.STROKE
         strokeWidth = dp(2f)
         color = Color.RED
+    }
+
+    private val hitReward = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.STROKE
+        strokeWidth = dp(1.5f)
+        color = Color.YELLOW
     }
 
     //Game Controls
@@ -49,6 +83,7 @@ class GameView @JvmOverloads constructor(
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
+        //Main Character
         mainCharacterW = w * 0.2f
         mainCharacterH = mainCharacterW
         mainCharacterBmp = mainCharacterBmpSrc.scale(mainCharacterW.toInt(), mainCharacterH.toInt())
@@ -59,6 +94,13 @@ class GameView @JvmOverloads constructor(
 
         //Initial vertical velocity
         mainCharacterVelY = 0f
+
+        //Reward
+        rewardWidth = w * 0.2f
+        rewardHeight = rewardWidth
+        rewardBmp = rewardsBmpSrc.scale(rewardWidth.toInt(), rewardHeight.toInt())
+
+        rewards.clear()
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -70,6 +112,7 @@ class GameView @JvmOverloads constructor(
         return super.onTouchEvent(event)
     }
 
+    @SuppressLint("DrawAllocation")
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
@@ -80,14 +123,44 @@ class GameView @JvmOverloads constructor(
         canvas.drawColor(Color.BLUE)
 
         if (running) {
+            //Main Character
             mainCharacterVelY += gravity
-            mainCharacterY =
-                (mainCharacterY + mainCharacterVelY).coerceIn(0f, height - mainCharacterH)
+            mainCharacterY = (mainCharacterY + mainCharacterVelY).coerceIn(0f, height - mainCharacterH)
+
+            //Rewards
+            if (Random.nextFloat() < rewardSpawnChance) {
+                val y = Random.nextFloat() * (height * 0.7f) + (height * 0.1f)
+                rewards += Reward(x = width + rewardWidth, y = y)
+            }
+
+            val it = rewards.iterator()
+            while (it.hasNext()) {
+                val r = it.next()
+                r.x -= rewardSpeed
+
+                //Sale de pantalla (Izquierda)
+                if (r.x + rewardWidth < 0f) {
+                    it.remove()
+                    continue
+                }
+
+                //Colision Main Character < -> Reward
+                if (checkMainCharacterRewardCollision(r)) {
+                    //counter ++
+                    it.remove()
+                }
+            }
         }
 
         //Draw Main Character
         mainCharacterBmp?.let {
             canvas.drawBitmap(it, mainCharacterX, mainCharacterY, null)
+        }
+
+        rewardBmp?.let { bmp ->
+            rewards.forEach { reward ->
+                canvas.drawBitmap(bmp, reward.x, reward.y, null)
+            }
         }
 
         //Hitbox
@@ -100,7 +173,19 @@ class GameView @JvmOverloads constructor(
                 mainCharacterH,
                 hitboxMainCharacter
             )
-            canvas.drawRect(mainCharacterRect, hitPaint)
+            canvas.drawRect(mainCharacterRect, hitMainCharacter)
+
+            rewards.forEach { reward ->
+                setCenteredRect(
+                    rewardRect,
+                    reward.x,
+                    reward.y,
+                    rewardWidth,
+                    rewardHeight,
+                    hitboxRewards
+                )
+                canvas.drawRect(rewardRect, hitReward)
+            }
         }
 
         if (running) postInvalidateOnAnimation()
@@ -116,5 +201,25 @@ class GameView @JvmOverloads constructor(
         out.top = y + (h - ch) / 2f
         out.right = out.left + cw
         out.bottom = out.top + ch
+    }
+
+    private fun checkMainCharacterRewardCollision(reward: Reward): Boolean {
+        setCenteredRect(
+            mainCharacterRect,
+            mainCharacterX,
+            mainCharacterY,
+            mainCharacterW,
+            mainCharacterH,
+            hitboxMainCharacter
+        )
+        setCenteredRect(
+            rewardRect,
+            reward.x,
+            reward.y,
+            rewardWidth,
+            rewardHeight,
+            hitboxRewards
+        )
+        return RectF.intersects(mainCharacterRect,rewardRect)
     }
 }
